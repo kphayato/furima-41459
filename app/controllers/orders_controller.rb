16cfe22edd1da731259fb5670783a30daa1d4b82
kphayato <hayato.k.0608@gmail.com>
2025-01-12 16:17:25 +0900

    Add remember_created_at to users for Devise rememberable

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,6 +25,12 @@ class OrdersController < ApplicationController
 
   private
 
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
+
   def set_item
     @item = Item.find(params[:item_id])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_31_015000) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_12_071707) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -95,6 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_31_015000) do
     t.date "birth_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "remember_created_at"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
